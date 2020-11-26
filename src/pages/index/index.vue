865e4b819f9e927ad01f866c5265b33ab0a5c305
sudaojun <1251662462@qq.com>
2020-11-26 22:25:33 +0800

    init

---
+++ src/pages/index/index.vue
@@ -0,0 +1,25 @@
+<template>
+	<view class="index-content">
+
+	</view>
+</template>
+
+<script>
+	export default {
+		data() {
+			return {
+
+			}
+		},
+		onLoad() {
+      
+		},
+		methods: {
+
+		}
+	}
+</script>
+
+<style lang="scss" scoped>
+	
+</style>
