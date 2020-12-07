e80bc704da59e53f9565ada93aec17c2d5a67ed9
sudaojun <1251662462@qq.com>
2020-12-07 18:23:48 +0800

    路由

---
+++ src/utils/route.js
@@ -0,0 +1,43 @@
+ const install = (Vue, vm) => {
+  const navigateTo = (path = '', params = {}) => { 
+    vm.$u.route({
+      type: 'navigateTo',
+      url: path,
+      params: params
+    })
+  }
+  const redirectTo = (path = '', params = {}) => {
+    vm.$u.route({
+      type: 'redirectTo',
+      url: path,
+      params: params
+    })
+  }
+  const switchTab = (path = '', params = {}) => {
+    vm.$u.route({
+      type: 'switchTab',
+      url: path,
+      params: params
+    })
+  }
+  const reLaunch = (path = '', params = {}) => {
+    vm.$u.route({
+      type: 'reLaunch',
+      url: path,
+      params: params
+    })
+  }
+  
+  const navigateBack = (delta = 1) => {
+    //可以存储上一页的查询参数？？
+    vm.$u.route({
+      type: 'navigateBack',
+      delta: delta
+    })
+  }
+  vm.$u.route = {redirectTo, navigateTo, navigateBack, reLaunch, switchTab}
+}
+
+export default {
+	install
+}
