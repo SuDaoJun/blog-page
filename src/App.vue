7897825c927c934e625521eeacdf24a32d639020
sudaojun <1251662462@qq.com>
2020-12-08 18:09:18 +0800

    公共背景

--- src/App.vue
+++ src/App.vue
@@ -15,5 +15,12 @@
 <style lang="scss">
   /*每个页面公共css */
   @import "uview-ui/index.scss";
+  @import "@/static/css/iconfont.css";
 
+  body{
+    background-color: #F7F7F7;
+    min-height: 100vh;
+    width: 100vw;
+    overflow-x: hidden;
+  }
 </style>
